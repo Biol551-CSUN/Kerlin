ba981df7cab6c00848ab5c3e19c857a4591ddd31
Jamie Kerlin <jamiekerlin@gmail.com>
2021-02-14 17:54:14 -0800

    fix comments in script

--- Week_3/Scripts/2021-02-10_lab_assignment.R
+++ Week_3/Scripts/2021-02-10_lab_assignment.R
@@ -3,6 +3,7 @@
 #Plotting penguin data from palmerpenguin package
 #Created by Jamie, Roland, and Jasmin
 #Created on 2021-02-10
+#Updated on 2021-02-14
 #######################################################################
 ### Load libraries #######
 library(palmerpenguins)
@@ -19,8 +20,7 @@ library(ggthemes)
 #Look at data using glimpse
 glimpse(penguins)
 
-
-#### Histogram ########################
+#### Create boxplot ##################################################
 penguins <- na.omit(penguins) #remove NAs in sex and species
 
 penguins <- penguins %>%
@@ -29,16 +29,16 @@ penguins <- penguins %>%
 ggplot(data = penguins, 
        mapping = aes(x = species, #compare species for bill depth (mm)
                      y = bill_depth_mm,
-                     fill = species), 
-       stat = "identity") +  #fill color species
+                     fill = species), #fill color species
+       stat = "identity") +  
   geom_boxplot() +
   facet_grid(~sex, switch = "x") + #facet for sex within species
   scale_fill_manual(values = pnw_palette(1)[c(3, 5, 7)]) + #PNW palette fill
-  labs(title = "Comparison of Bill Depth (mm) Across Species Within Sex", #titles, axes, legends
-       x = "Species",
-       y = "Bill depth (mm)",
-       fill = "Species",
-       caption = "Source: Palmer Station LTER / palmerpenguins packages") +
+  labs(title = "Comparison of Bill Depth (mm) Across Species Within Sex", #add title
+       x = "Species", #change x axis label
+       y = "Bill depth (mm)", #change y axis label
+       fill = "Species", #change legend label
+       caption = "Source: Palmer Station LTER / palmerpenguins packages") + #add caption
   theme_classic() + #change theme
   theme(axis.title.x = element_text(color = "deepskyblue4", face = 2), #change axis text color
         axis.title.y = element_text(color = "deepskyblue4", face = 2)) +
