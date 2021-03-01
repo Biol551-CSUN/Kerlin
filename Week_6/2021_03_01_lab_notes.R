1d5bb265946a24902038d06628f4d619d1a9f3c4
Jamie Kerlin <jamiekerlin@gmail.com>
2021-03-01 09:27:41 -0800

    2021_03_01 Rmarkdown

---
+++ Week_6/2021_03_01_lab_notes.R
@@ -0,0 +1,8 @@
+### R Markdown Lab #######################################
+### Working with R Markdown Language
+### Created by Jamie Kerlin
+### Created on 2021-03-01
+##########################################################
+
+### Load libraries #######################################
+library(tinytex)
