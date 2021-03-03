b33f5bbfbeb8ed681938db3a5599d91c90d97d90
Jamie Kerlin <jamiekerlin@gmail.com>
2021-03-03 11:33:47 -0800

    week 6 file name change

--- Week_6/Scripts/2021_03_01_lab_notes.R
+++
@@ -1,8 +0,0 @@
-### R Markdown Lab #######################################
-### Working with R Markdown Language
-### Created by Jamie Kerlin
-### Created on 2021-03-01
-##########################################################
-
-### Load libraries #######################################
-library(tinytex)
