e00dceeb93e2b703516426b98befe2581bf42438
Jamie Kerlin <jamiekerlin@gmail.com>
2021-04-29 20:00:54 -0700

    week 13

---
+++ Week_13/Scripts/2021_04_28_lab_notes.R
@@ -0,0 +1,15 @@
+### Iterative coding ######################################
+# Created on 2021-04-26
+# Created by Jamie Kerlin
+###########################################################
+
+### Load libraries ########################################
+library(tidyverse)
+library(here)
+library(modelsummary)
+library(tidymodels)
+library(broom)
+library(flextable)
+library(palmerpenguins)
+library(performance)
+
